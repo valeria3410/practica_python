d4dd150db0750b4c2e7aa7938338264cb2325ba8
valeria3410 <valeriaalbornoz_34_10@hotmail.com>
2023-09-23 20:26:12 -0300

    Finalización ejercicio seis

--- 5.py
+++ 5.py
@@ -1,7 +1,11 @@
 edad= int (input("Ingrese su edad "))
 if edad >= 18:
        print ("Puede ingresar")
+       if edad >= 25:
+            print ("Si puede ingresar al Vip")
+       else:
+            print ("Puede ingrsar, pero no al Vip")
 else:
-    print ("No puede ingresar")
+       print ("No puede ingresar")
 input()
 
