6755e05260bef35ab12222ad2ef57e4da57edec5
valeria3410 <valeriaalbornoz_34_10@hotmail.com>
2023-09-23 19:30:43 -0300

    Nuevo ejercio cuatro

---
+++ 4.py
@@ -0,0 +1,9 @@
+nota1= int (input ("nota una "))
+nota2= int (input("nota dos "))
+promedio= (nota1 + nota2/2)
+if promedio >=6:
+    print ("Aprueba")
+else:
+    print("Desaprueba")
+input()
+    
